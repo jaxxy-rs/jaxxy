af2daf4ec04934bee1d318fed5bffe5d4d9c897a
James Carman <jwcarman@gmail.com>
2018-09-01 09:23:04 -0400

    Adding request log filter.

---
+++ logging/src/main/java/org/jaxxy/logging/RequestLogFilter.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2018 The Jaxxy Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jaxxy.logging;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.util.Optional.ofNullable;
+
+@Provider
+@Priority(Priorities.USER - 100)
+@Builder
+public class RequestLogFilter implements ContainerRequestFilter, ContainerResponseFilter {
+//----------------------------------------------------------------------------------------------------------------------
+// Fields
+//----------------------------------------------------------------------------------------------------------------------
+
+    private static final String DEFAULT_ELAPSED_TIME_HEADER = "X-Elapsed-Time";
+    private static final String BEGIN_TS_PROP = RequestLogFilter.class.getCanonicalName() + ".beginTs";
+
+    @Builder.Default
+    private final Logger logger = LoggerFactory.getLogger(RequestLogFilter.class);
+
+    @Builder.Default
+    private final String elapsedTimeHeader = DEFAULT_ELAPSED_TIME_HEADER;
+
+    @Builder.Default
+    private final TimeUnit elapsedTimeUnit = TimeUnit.SECONDS;
+
+    @Getter(value= AccessLevel.PRIVATE, lazy=true)
+    private final double divisor = TimeUnit.NANOSECONDS.convert(1, elapsedTimeUnit);
+
+//----------------------------------------------------------------------------------------------------------------------
+// ContainerRequestFilter Implementation
+//----------------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+        requestContext.setProperty(BEGIN_TS_PROP, System.nanoTime());
+        if (logger.isInfoEnabled()) {
+            logger.info("BEGIN {} {}", requestContext.getMethod(), requestContext.getUriInfo().getAbsolutePath().getPath());
+        }
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+// ContainerResponseFilter Implementation
+//----------------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        ofNullable((Long) requestContext.getProperty(BEGIN_TS_PROP)).ifPresent(beginTs->{
+            final long elapsedNanos = System.nanoTime() - beginTs;
+            final Response.StatusType statusInfo = responseContext.getStatusInfo();
+            final String elapsedText = String.format("%1.3f %s", elapsedNanos / getDivisor(), elapsedTimeUnit.name().toLowerCase());
+            responseContext.getHeaders().putSingle(elapsedTimeHeader, elapsedText);
+            if (logger.isInfoEnabled()) {
+                logger.info("END   {} {} - {} {} ({})", requestContext.getMethod(), requestContext.getUriInfo().getAbsolutePath().getPath(), statusInfo.getStatusCode(), statusInfo.getReasonPhrase(), elapsedText);
+            }
+        });
+    }
+}
---
+++ logging/src/test/java/org/jaxxy/logging/RequestLogFilterTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2018 The Jaxxy Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jaxxy.logging;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jaxxy.test.JaxrsServerConfig;
+import org.jaxxy.test.JaxrsTestCase;
+import org.jaxxy.test.hello.DefaultHelloResource;
+import org.jaxxy.test.hello.HelloResource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RequestLogFilterTest extends JaxrsTestCase<HelloResource> {
+//----------------------------------------------------------------------------------------------------------------------
+// Fields
+//----------------------------------------------------------------------------------------------------------------------
+
+    @Mock
+    private Logger log;
+
+    @Captor
+    private ArgumentCaptor<String> elapsedTimeCaptor;
+
+//----------------------------------------------------------------------------------------------------------------------
+// Other Methods
+//----------------------------------------------------------------------------------------------------------------------
+
+    @Override
+    protected void configureServer(JaxrsServerConfig config) {
+        config.withProvider(RequestLogFilter.builder()
+                .elapsedTimeUnit(TimeUnit.MILLISECONDS)
+                .logger(log)
+                .build());
+    }
+
+    @Override
+    protected HelloResource createServiceObject() {
+        return new DefaultHelloResource();
+    }
+
+    @Test
+    public void testElapsedTimeHeader() {
+        final Response response = webTarget().path("hello").path("Jaxxy").request(MediaType.TEXT_PLAIN_TYPE).get();
+        assertThat(response.getHeaderString("X-Elapsed-Time")).isNotNull();
+    }
+
+    @Test
+    public void testLoggingWhenDisabled() {
+        when(log.isInfoEnabled()).thenReturn(false);
+        clientProxy().sayHello("Jaxxy");
+        verify(log, times(2)).isInfoEnabled();
+        verifyNoMoreInteractions(log);
+    }
+
+    @Test
+    public void testLoggingWhenEnabled() {
+        when(log.isInfoEnabled()).thenReturn(true);
+        clientProxy().sayHello("Jaxxy");
+        verify(log, times(2)).isInfoEnabled();
+        verify(log).info("BEGIN {} {}", "GET", "/hello/Jaxxy");
+        verify(log).info(eq("END   {} {} - {} {} ({})"), eq("GET"), eq("/hello/Jaxxy"), eq(200), eq("OK"), elapsedTimeCaptor.capture());
+        assertThat(elapsedTimeCaptor.getValue()).matches("\\d+\\.\\d\\d\\d milliseconds");
+        verifyNoMoreInteractions(log);
+    }
+}
